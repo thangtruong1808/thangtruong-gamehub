a95041f2acc85dc7d768c68dbc88d79bd1f2ff08
Thang-Truong <thangtruong1808@gmail.com>
2023-12-21 13:27:54 +1100

    Installed ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import reactLogo from './assets/react.svg'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 import './App.css'
 
 function App() {
-
   return (
-   <>
-  </>
+    <Button colorScheme='blue'>Button</Button>
   )
 }
 
